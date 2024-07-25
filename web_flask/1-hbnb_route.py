e2bbf9a8450cddf89c494cca627e89a2ef920013
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-25 09:49:39 -0700

    display text in URL

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask
+"""Flask web application module."""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Route handler for the root URL. returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def only_HBNB():
+    """Route handler for the /hbnb URL. returns HBNB!"""
+    return "HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+"""Flask web application module."""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Route handler for the root URL. returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def only_HBNB():
+    """Route handler for the /hbnb URL. returns HBNB!"""
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def describe_c(text):
+    """Route handler for the /c/<text> URL. displays text after c character"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
