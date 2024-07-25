eae6d1c1dd026a048e287844babeb8c74f3d695a
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-24 21:54:03 -0700

    create a simple app that returns a greeting

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+"""Flask web application module."""
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_HBNB():
+    """Route handler for the root URL. returns Hello HBNB!$"""
+    return "Hello HBNB!$"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
