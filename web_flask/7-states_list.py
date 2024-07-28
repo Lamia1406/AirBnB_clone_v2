4a8802d28884244eb46376ac375138b2a93b4c1d
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-28 03:22:22 -0700

    display states list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Flask web application module."""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """
+    Route handler for the /states_list/ URL.
+    displays states list in ul list
+    """
+    return render_template('7-states_list.html', states=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
