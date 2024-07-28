35690e0f4662e8a43c443ebdcf52ac464d8704e7
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-28 05:20:20 -0700

    correct modules

--- models/city.py
+++ models/city.py
@@ -18,6 +18,7 @@ class City(BaseModel, Base):
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         state = relationship("State", back_populates="cities")
+        places = relationship("Place", backref="cities")
     else:
         name = ""
         state_id = ""
