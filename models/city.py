20f85fbb5aca507349d1535f2f031dc1a059c882
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-28 02:49:57 -0700

    correct error

--- models/city.py
+++ models/city.py
@@ -4,15 +4,20 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-
     places = relationship(
             "Place",
             cascade='all, delete, delete-orphan',
             backref="cities")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship("State", back_populates="cities")
+    else:
+        name = ""
+        state_id = ""
